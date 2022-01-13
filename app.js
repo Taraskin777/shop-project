2a051821f08f370051e32af90971f38487e408fb
Taras <taraskin2012@gmail.com>
2022-01-13 18:48:38 +0200

    add modal window for more details

--- app.js
+++ app.js
@@ -6,7 +6,52 @@ console.log(addToCartBtns);
 
 
 for (let i = 0; i < addToCartBtns.length; i++) {
-   addToCartBtns[i].addEventListener("click", function () {
-     console.log("clicked");
-   });
- }
+  addToCartBtns[i].addEventListener("click", function () {
+    //  console.log("clicked");
+
+    // console.log(prevProductCount);
+
+    productsCountEl.textContent = +productsCountEl.textContent + 1;
+
+  });
+}
+
+
+
+let productLike = document.querySelectorAll(".heart");
+for (let i = 0; i < productLike.length; i++) {
+  productLike[i].addEventListener("click", function () {
+    console.log("like");
+  });
+};
+
+productLike.forEach((item) => {
+  item.addEventListener("click", function () {
+    if (item.classList.contains("liked")) {
+      item.classList.remove("liked");
+    } else {
+      item.classList.add("liked");
+    }
+  });
+
+});
+
+// modal
+
+let detailsBtn = document.querySelectorAll(".more");
+console.log(detailsBtn);
+
+let modalWindow = document.querySelector(".modal");
+console.log(modalWindow)
+
+let btnClose = document.querySelector(".btn-close");
+
+detailsBtn.forEach((item) => {
+  item.addEventListener("click", function () {
+    modalWindow.classList.add("show");
+  });
+
+  btnClose.addEventListener("click", function () {
+    modalWindow.classList.remove("show");
+  });
+});
