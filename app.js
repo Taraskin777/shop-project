73a4d9ca737ead446615a9541a05b581a1217849
Taras <taraskin2012@gmail.com>
2022-01-16 20:07:41 +0200

    add slider

--- app.js
+++ app.js
@@ -1,37 +1,39 @@
+
+//add to cart
 const productsCountEl = document.getElementById("products-count");
-console.log(productsCountEl);
-
 const addToCartBtns = document.querySelectorAll(".add");
-console.log(addToCartBtns);
-
 
 for (let i = 0; i < addToCartBtns.length; i++) {
   addToCartBtns[i].addEventListener("click", function () {
-    //  console.log("clicked");
-
-    // console.log(prevProductCount);
-
+  
     productsCountEl.textContent = +productsCountEl.textContent + 1;
 
   });
 }
 
-
+// add and remove like
 
 let productLike = document.querySelectorAll(".heart");
-for (let i = 0; i < productLike.length; i++) {
-  productLike[i].addEventListener("click", function () {
-    console.log("like");
-  });
-};
+// for (let i = 0; i < productLike.length; i++) {
+//   productLike[i].addEventListener("click", function () {
+//     console.log("like");
+//   });
+// };
+
+// productLike.forEach(item => {
+//   item.addEventListener('click', function () {
+//     console.log('liked');
+//   });
+// });
 
 productLike.forEach((item) => {
   item.addEventListener("click", function () {
-    if (item.classList.contains("liked")) {
-      item.classList.remove("liked");
-    } else {
-      item.classList.add("liked");
-    }
+    // if (item.classList.contains("liked")) {
+    //   item.classList.remove("liked");
+    // } else {
+    //   item.classList.add("liked");
+    // }
+    item.classList.toggle('liked'); //або
   });
 
 });
@@ -42,16 +44,39 @@ let detailsBtn = document.querySelectorAll(".more");
 console.log(detailsBtn);
 
 let modalWindow = document.querySelector(".modal");
-console.log(modalWindow)
+console.log(modalWindow);
 
 let btnClose = document.querySelector(".btn-close");
 
 detailsBtn.forEach((item) => {
   item.addEventListener("click", function () {
     modalWindow.classList.add("show");
+    modalWindow.classList.remove('hide');
   });
 
   btnClose.addEventListener("click", function () {
     modalWindow.classList.remove("show");
+    modalWindow.classList.add('hide');
   });
+});
+
+
+
+   $ (".slider").slick({
+      dots: true,
+      autoplay: true,     
+   });
+  
+// модальне вікно при прокрутці сторінки 
+
+
+
+window.addEventListener('scroll', function() {
+  let scrolled = window.pageYOffset;
+  console.log(scrolled);
+  
+  if (scrolled > 800) {
+    modalWindow.classList.add("show");
+    modalWindow.classList.remove('hide');
+  };
 });
